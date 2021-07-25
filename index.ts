751f21bbfc8a492601db048f551ea0536199ce69
Josian Godard <josian.godard@webedia-group.com>
2021-07-25 10:30:15 +0200

    export source types

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { default as PreviewWrapper } from "./src/components/PreviewWrapper";
 export { default as usePreview } from "./src/hooks/usePreview";
+export * from "@hey_joz/gatsby-source-netlify-cms/src/types";
--- src/types.ts
+++ src/types.ts
@@ -1,45 +1,5 @@
 import type { PluginOptions as OrigPluginOptions } from "gatsby";
-import { CmsCollection, InitOptions } from "netlify-cms-core";
-
-export type CmsCollectionFile = Exclude<
-  CmsCollection["files"],
-  undefined
->[number];
-
-export type CmsCollectionField = Exclude<
-  CmsCollection["fields"],
-  undefined
->[number];
-
-export type CmsCollectionFieldObject = Extract<
-  CmsCollectionField,
-  { widget: "object" }
->;
-
-export type CmsCollectionFieldList = Extract<
-  CmsCollectionField,
-  { widget: "list" }
->;
-
-export type CmsCollectionFieldNumber = Extract<
-  CmsCollectionField,
-  { widget: "number" }
->;
-
-export type CmsCollectionFieldRelation = Extract<
-  CmsCollectionField,
-  { widget: "relation" }
->;
-
-export type CmsCollectionFieldBoolean = Extract<
-  CmsCollectionField,
-  { widget: "boolean" }
->;
-
-export type CmsCollectionFieldSelect = Extract<
-  CmsCollectionField,
-  { widget: "select" }
->;
+import { InitOptions } from "netlify-cms-core";
 
 export type PluginOptions = OrigPluginOptions & { initOptions: InitOptions };
 
