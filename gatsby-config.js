e4ba369c9fcdfec1e9e81d6ae1ce4bcde4f5c82e
Josian Godard <josian.godard@webedia-group.com>
2021-08-02 22:34:25 +0200

    remove filesystem stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
-module.exports = ({ initOptions }) => ({
+module.exports = ({ initOptions, pageCollectionNames }) => ({
   plugins: [
     {
       resolve: "@hey_joz/gatsby-source-netlify-cms",
       options: {
         initOptions,
+        pageCollectionNames,
       },
     },
   ],
