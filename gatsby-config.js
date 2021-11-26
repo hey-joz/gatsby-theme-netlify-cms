f3c96b9e9816adb49e93a84335293b2d6659476c
Josian Godard <josian.godard@webedia-group.com>
2021-11-26 22:17:55 +0100

    Change deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,3 @@
-module.exports = ({ initOptions, pageCollectionNames }) => ({
-  plugins: [
-    {
-      resolve: "@hey_joz/gatsby-source-netlify-cms",
-      options: {
-        initOptions,
-        pageCollectionNames,
-      },
-    },
-  ],
+module.exports = () => ({
+  plugins: [],
 });
